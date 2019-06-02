7853a49d38bc6c932ca7b40d5255e2da96b66ae8
Mike Lambert <cyberspacelabs@gmail.com>
2019-06-02 18:46:11 +0300

    initial commit

---
+++ src/main/java/com/cyfrant/TelegramChannelFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.cyfrant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramChannelFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramChannelFrontendApplication.class, args);
+	}
+
+}
