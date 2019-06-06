a616d594eeae305733eccabe76c331bc0b218dfc
Mike Lambert <cyberspacelabs@gmail.com>
2019-06-06 21:11:52 +0300

    fixing protocol-independent links

--- src/main/java/com/cyfrant/tgfrontend/service/impl/PageService.java
+++ src/main/java/com/cyfrant/tgfrontend/service/impl/PageService.java
@@ -172,6 +172,9 @@ public class PageService {
                 String attribute = "href";
                 try {
                     String link = r.attr(attribute);
+                    if (link.startsWith("//")) {
+                        link = "https://" + link;
+                    }
                     String css = dataUriService.content(link);
                     css = bundleCssBackground(css);
                     String embed = "data:text/css;base64," + Base64Utils.encodeToString(
